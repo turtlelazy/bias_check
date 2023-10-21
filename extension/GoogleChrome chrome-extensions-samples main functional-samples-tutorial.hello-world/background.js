5a25ddd21d648bd6a3ad990bce913e8c6febd398
turtlelazy <ishraqmahid@gmail.com>
2023-10-21 11:16:16 -0400

    extension sandbox

---
+++ extension/GoogleChrome chrome-extensions-samples main functional-samples-tutorial.hello-world/background.js
@@ -0,0 +1,6 @@
+function curr_window(){
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+        let url = tabs[0].url;
+        // use `url` here inside the callback because it's asynchronous!
+    });
+}
---
+++ extension/GoogleChrome chrome-extensions-samples main functional-samples-tutorial.hello-world/scripts/content.js
@@ -0,0 +1,9 @@
+class_name = "LC20lb MBeuO DKV0Md";
+const article = document.getElementsByClassName(class_name);
+
+if(article){
+    for(let i = 0; i < article.length;i++){
+        
+    }
+    console.log("something aboard!");
+}
