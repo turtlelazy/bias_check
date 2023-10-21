8c2556f1410f38a153a4cc3ed0d1cce5303b351a
Miles Acquaviva <milesacq@icloud.com>
2023-10-21 13:22:55 -0400

    getting data

--- extension/GoogleChrome chrome-extensions-samples main functional-samples-tutorial.hello-world/scripts/content.js
+++ extension/GoogleChrome chrome-extensions-samples main functional-samples-tutorial.hello-world/scripts/content.js
@@ -1,9 +1,35 @@
-class_name = "LC20lb MBeuO DKV0Md";
-const article = document.getElementsByClassName(class_name);
+// insert_info();
+// searchForLink(document.body);
+url = chrome.runtime.getURL("aggregate_data.json");
+let data
+fetch(url)
+  .then((response) => response.json()) // file contains json
+  .then((json) => { 
+    data = json
+    console.log(data)
+    searchForLink(document.body)}
+    );
+  
+function searchForLink(tag) {
 
-if(article){
-    for(let i = 0; i < article.length;i++){
-        
+  if (tag.nodeName == "A") {
+    console.log(tag)
+    link = tag.href;
+    sourceData = getData(link);
+    const dataContainer = document.createElement("div");
+    element.appendChild(dataContainer);
+    dataContainer.appendChild(document.createTextNode(sourceData));
+  } else {
+    Array.from(tag.children).forEach((child) => searchForLink(child));
+  }
+}
+
+function getData(link) {
+    // console.log(data)
+  for (source in data) {
+    if (link.includes(source)) {
+        console.log("hit")
+        return data[source]
     }
-    console.log("something aboard!");
+  }
 }
