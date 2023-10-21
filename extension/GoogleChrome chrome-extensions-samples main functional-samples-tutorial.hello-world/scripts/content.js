6220d731cce49bbf4e13a19f4764acd133c0050a
Miles Acquaviva <milesacq@icloud.com>
2023-10-21 13:46:44 -0400

    ratings showing

--- extension/GoogleChrome chrome-extensions-samples main functional-samples-tutorial.hello-world/scripts/content.js
+++ extension/GoogleChrome chrome-extensions-samples main functional-samples-tutorial.hello-world/scripts/content.js
@@ -1,35 +1,55 @@
 // insert_info();
 // searchForLink(document.body);
 url = chrome.runtime.getURL("aggregate_data.json");
-let data
+let data;
 fetch(url)
   .then((response) => response.json()) // file contains json
-  .then((json) => { 
-    data = json
-    console.log(data)
-    searchForLink(document.body)}
-    );
-  
-function searchForLink(tag) {
+  .then((json) => {
+    data = json;
+    console.log(data);
+    searchForLink(document.body);
+  });
 
+function searchForLink(tag) {
   if (tag.nodeName == "A") {
-    console.log(tag)
+    console.log(tag);
     link = tag.href;
     sourceData = getData(link);
-    const dataContainer = document.createElement("div");
-    element.appendChild(dataContainer);
-    dataContainer.appendChild(document.createTextNode(sourceData));
+    if (sourceData != "miss") {
+      const dataContainer = document.createElement("div");
+      tag.appendChild(dataContainer);
+      let img = new Image();
+      if (sourceData.Report.Bias == "LEAST BIASED") {
+        img.src = chrome.runtime.getURL("0.png");
+      } else if (sourceData.Report.Bias == "LEFT-CENTER") {
+        img.src = chrome.runtime.getURL("-1.png");
+      } else if (sourceData.Report.Bias == "LEFT") {
+        img.src = chrome.runtime.getURL("-2.png");
+      } else if (sourceData.Report.Bias == "EXTREME LEFT") {
+        img.src = chrome.runtime.getURL("-3.png");
+      } else if (sourceData.Report.Bias == "EXTREME RIGHT") {
+        img.src = chrome.runtime.getURL("3.png");
+      } else if (sourceData.Report.Bias == "RIGHT-CENTER") {
+        img.src = chrome.runtime.getURL("1.png");
+      } else if (sourceData.Report.Bias == "RIGHT") {
+        img.src = chrome.runtime.getURL("2.png");
+      }
+      
+      img.style.width = "10vw";
+      dataContainer.appendChild(img);
+    }
   } else {
     Array.from(tag.children).forEach((child) => searchForLink(child));
   }
 }
 
 function getData(link) {
-    // console.log(data)
+  // console.log(data)
   for (source in data) {
     if (link.includes(source)) {
-        console.log("hit")
-        return data[source]
+      console.log("hit");
+      return data[source];
     }
   }
+  return "miss";
 }
